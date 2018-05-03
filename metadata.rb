0c97f91f71555be6abf1b307bef01b5cee9b4a99
Linus Ruth <linus.ruth@changepoint.com>
2018-05-03 16:42:43 -0700

    chore: increment cookbook minor version in metadata

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ chef_version     '>= 12.6' if respond_to?(:chef_version)
 license          'Apache-2.0'
 description      'Installs/Configures Notepad++'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
 supports         'windows'
 depends          'windows'
