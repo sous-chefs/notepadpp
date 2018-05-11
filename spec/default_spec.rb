0cc83058782e829c27c064fabc69a57a75ecb9f6
Linus Ruth <linus.ruth@changepoint.com>
2018-05-11 16:56:50 -0700

    fix: architecture specific package names in tests

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -6,7 +6,7 @@ describe 'notepadpp::default' do
       end.converge(described_recipe)
     end
     it 'downloads the Notepadd++ package' do
-      expect(chef_run).to install_windows_package('Notepad++')
+      expect(chef_run).to install_windows_package('Notepad++ (32-bit x86)')
         .with(
           source: 'https://notepad-plus-plus.org/repository/7.x/7.5.6/npp.7.5.6.Installer.exe',
           checksum: '9ce95883c5821c3fa7b9319f4952d67ae077cdd79d5327dd7bed63542703cb82'
@@ -21,7 +21,7 @@ describe 'notepadpp::default' do
       end.converge(described_recipe)
     end
     it 'downloads the Notepadd++ package' do
-      expect(chef_run).to install_windows_package('Notepad++')
+      expect(chef_run).to install_windows_package('Notepad++ (64-bit x64)')
         .with(
           source: 'https://notepad-plus-plus.org/repository/7.x/7.5.6/npp.7.5.6.Installer.x64.exe',
           checksum: 'adc915baa76e80c26d04f0ce1df6b592da809b3e14815f5a53369a7f3a993a83'
